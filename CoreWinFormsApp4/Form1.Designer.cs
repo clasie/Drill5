bc1d18cc90ae97c23839e3fb8a94b76e72abb247
clasie <claude_siefers@hotmail.com>
2022-10-26 14:52:27 +0200

    KOff

---
+++ CoreWinFormsApp4/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace CoreWinFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_testConnection = new System.Windows.Forms.Button();
+            this.btn_getData = new System.Windows.Forms.Button();
+            this.btn_insertData = new System.Windows.Forms.Button();
+            this.btn_updateData = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_testConnection
+            // 
+            this.btn_testConnection.Location = new System.Drawing.Point(208, 85);
+            this.btn_testConnection.Name = "btn_testConnection";
+            this.btn_testConnection.Size = new System.Drawing.Size(261, 29);
+            this.btn_testConnection.TabIndex = 0;
+            this.btn_testConnection.Text = "Test Connection";
+            this.btn_testConnection.UseVisualStyleBackColor = true;
+            this.btn_testConnection.Click += new System.EventHandler(this.btn_testConnection_Click);
+            // 
+            // btn_getData
+            // 
+            this.btn_getData.Location = new System.Drawing.Point(208, 134);
+            this.btn_getData.Name = "btn_getData";
+            this.btn_getData.Size = new System.Drawing.Size(261, 29);
+            this.btn_getData.TabIndex = 1;
+            this.btn_getData.Text = "Get data";
+            this.btn_getData.UseVisualStyleBackColor = true;
+            this.btn_getData.Click += new System.EventHandler(this.btn_getData_Click);
+            // 
+            // btn_insertData
+            // 
+            this.btn_insertData.Location = new System.Drawing.Point(208, 181);
+            this.btn_insertData.Name = "btn_insertData";
+            this.btn_insertData.Size = new System.Drawing.Size(261, 29);
+            this.btn_insertData.TabIndex = 2;
+            this.btn_insertData.Text = "InsertData";
+            this.btn_insertData.UseVisualStyleBackColor = true;
+            this.btn_insertData.Click += new System.EventHandler(this.btn_insertData_Click);
+            // 
+            // btn_updateData
+            // 
+            this.btn_updateData.Location = new System.Drawing.Point(208, 226);
+            this.btn_updateData.Name = "btn_updateData";
+            this.btn_updateData.Size = new System.Drawing.Size(261, 29);
+            this.btn_updateData.TabIndex = 3;
+            this.btn_updateData.Text = "UpdateData";
+            this.btn_updateData.UseVisualStyleBackColor = true;
+            this.btn_updateData.Click += new System.EventHandler(this.btn_updateData_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(208, 275);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(261, 29);
+            this.btn_delete.TabIndex = 4;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_updateData);
+            this.Controls.Add(this.btn_insertData);
+            this.Controls.Add(this.btn_getData);
+            this.Controls.Add(this.btn_testConnection);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_testConnection;
+        private System.Windows.Forms.Button btn_getData;
+        private System.Windows.Forms.Button btn_insertData;
+        private System.Windows.Forms.Button btn_updateData;
+        private System.Windows.Forms.Button btn_delete;
+    }
+}
+
---
+++ CoreWinFormsApp4/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoreWinFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var logingUI = new LoginUI();
+            logingUI.ShowDialog();
+            if (!logingUI.IsLogged)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btn_testConnection_Click(object sender, EventArgs e)
+        {
+            SqlConnection cnn = getConnection();
+            try
+            {
+                cnn.Open();
+                MessageBox.Show("Conn ok");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally {
+                if (cnn != null) {
+                    cnn.Close();
+                }
+            }
+        }
+        private void btn_getData_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection = getConnection();
+            try
+            {
+                sqlConnection.Open();
+                var request = "SELECT * FROM aviondeaf";
+                SqlCommand sqlCommand = new SqlCommand(request, sqlConnection);
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+                StringBuilder sb = new StringBuilder();
+                while (sqlDataReader.Read())
+                {
+                    sb.Append($"{sqlDataReader.GetValue(0)} - {sqlDataReader.GetValue(1)}").AppendLine();
+                }
+                MessageBox.Show(sb.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                if (sqlConnection != null)
+                {
+                    sqlConnection.Close();
+                }
+            }
+        }
+        private void btn_insertData_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection = getConnection();
+            try
+            {
+                sqlConnection.Open();
+                string 
+                    req = $"INSERT INTO aviondeaf (immat,typeAvion, nbHVol)";
+                    req += $"VALUES('CSI-3','csi3-avion',15)";
+
+                SqlCommand sqlCommand = new SqlCommand(req, sqlConnection);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+
+                sqlDataAdapter.InsertCommand = sqlCommand;
+                sqlDataAdapter.InsertCommand.ExecuteNonQuery();
+
+                sqlCommand.Dispose();
+                MessageBox.Show("InsertAjouté");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                if (sqlConnection != null)
+                {
+                    sqlConnection.Close();
+                }
+            }
+        }
+        private void btn_updateData_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection = getConnection();
+            try
+            {
+                sqlConnection.Open();
+                var req  = "UPDATE aviondeaf SET immat = 'CSI-Update1', typeAvion = 'typeAvion-Update' ";
+                req += " WHERE nbHVol = 13";
+                SqlCommand sqlCommand = new SqlCommand(req, sqlConnection);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+
+                sqlDataAdapter.UpdateCommand = sqlCommand;
+                sqlDataAdapter.UpdateCommand.ExecuteNonQuery();
+                sqlCommand.Dispose();
+
+                MessageBox.Show("Update ok");
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.ToString());
+            }
+            finally {
+                if (sqlConnection != null) {
+                    sqlConnection.Close();
+                }
+            }
+        }
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection = getConnection();
+            try
+            {
+                sqlConnection.Open();
+                sqlConnection.BeginTransaction();
+                var transaction = sqlConnection.BeginTransaction();
+                transaction.Commit();
+                transaction.Rollback();
+                var req = "DELETE FROM aviondeaf WHERE immat LIKE '%CSI%' ";
+                SqlCommand sqlCommand = new SqlCommand(req, sqlConnection);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+                sqlDataAdapter.DeleteCommand = sqlCommand;
+                sqlDataAdapter.DeleteCommand.ExecuteNonQuery();
+                sqlCommand.Dispose();
+                MessageBox.Show("Ok delete");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            finally {
+                if (sqlConnection != null) {
+                    sqlConnection.Close();
+                }
+            }
+        }
+        //Utils
+        private SqlConnection getConnection()
+        {
+            var connString = "Server=PC1224;DataBase=Aviation;Trusted_Connection=True";
+            SqlConnection cnn = new SqlConnection(connString);
+            return cnn;
+        }
+
+
+    }
+}

2e97bc12ff35cd59db7f860b52ed58e7e36eb83b
clasie <claude_siefers@hotmail.com>
2022-10-27 12:40:15 +0200

    Ajout transactions partout

--- DB/DataBase.cs
+++ DB/DataBase.cs
@@ -38,89 +38,129 @@ namespace DBase
         }
         public static string GetData(ref string err)
         {
-            SqlConnection sqlConnection = DBase.DataBase.GetConnection();
-            try
+            StringBuilder sb = new StringBuilder();
+            using (SqlConnection connection = DBase.DataBase.GetConnection())
             {
-                sqlConnection.Open();
-                var request = "SELECT * FROM aviondeaf";
-                SqlCommand sqlCommand = new SqlCommand(request, sqlConnection);
-                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
-                StringBuilder sb = new StringBuilder();
-                while (sqlDataReader.Read())
+                connection.Open();
+
+                SqlCommand command = connection.CreateCommand();
+                command.Connection = connection;
+                string req = "SELECT * FROM aviondeaf";
+                try
                 {
-                    sb.Append($"{sqlDataReader.GetValue(0)} - {sqlDataReader.GetValue(1)}").AppendLine();
+                    command.CommandText = req;
+                    SqlDataReader sqlDataReader = command.ExecuteReader();
+                    while (sqlDataReader.Read())
+                    {
+                        sb.Append($"{sqlDataReader.GetValue(0)} - {sqlDataReader.GetValue(1)}").AppendLine();
+                    }
+                    return sb.ToString();
                 }
-                return sb.ToString();
-            }
-            catch (Exception ex)
-            {
-                err = ex.ToString();
-                throw new Exception(err);
-            }
-            finally
-            {
-                if (sqlConnection != null)
+                catch (Exception ex)
                 {
-                    sqlConnection.Close();
+                    err = ex.ToString();
+                    return sb.ToString();
                 }
             }
         }
         public static void InsertData(ref string err)
         {
-            SqlConnection sqlConnection = DBase.DataBase.GetConnection();
-            try
+            using (SqlConnection connection = DBase.DataBase.GetConnection())
             {
-                sqlConnection.Open();
+                connection.Open();
+
+                SqlCommand command = connection.CreateCommand();
+                SqlTransaction transaction;
+
+                // Start a local transaction.
+                transaction = connection.BeginTransaction("SampleTransaction");
+
+                // Must assign both transaction object and connection 
+                // to Command object for a pending local transaction
+                command.Connection = connection;
+                command.Transaction = transaction;
+
                 string
-                    req = $"INSERT INTO aviondeaf (immat,typeAvion, nbHVol)";
-                req += $"VALUES('CSI-3','csi3-avion',15)";
+                     req = $"INSERT INTO aviondeaf (immat,typeAvion, nbHVol)";
+                     req += $"VALUES('CSI-3','csi3-avion',15)";
 
-                SqlCommand sqlCommand = new SqlCommand(req, sqlConnection);
-                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
-
-                sqlDataAdapter.InsertCommand = sqlCommand;
-                sqlDataAdapter.InsertCommand.ExecuteNonQuery();
-
-                sqlCommand.Dispose();
-            }
-            catch (Exception ex)
-            {
-                err = ex.ToString();
-                throw new Exception(err);
-            }
-            finally
-            {
-                if (sqlConnection != null)
+                try
                 {
-                    sqlConnection.Close();
+                    command.CommandText = req;
+                    command.ExecuteNonQuery();
+
+                    // Attempt to commit the transaction.
+                    transaction.Commit();
+                    Console.WriteLine("Both records are written to database.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Commit Exception Type: {0}", ex.GetType());
+                    Console.WriteLine("  Message: {0}", ex.Message);
+
+                    // Attempt to roll back the transaction. 
+                    try
+                    {
+                        transaction.Rollback();
+                    }
+                    catch (Exception ex2)
+                    {
+                        // This catch block will handle any errors that may have occurred 
+                        // on the server that would cause the rollback to fail, such as 
+                        // a closed connection.
+                        Console.WriteLine("Rollback Exception Type: {0}", ex2.GetType());
+                        Console.WriteLine("  Message: {0}", ex2.Message);
+                    }
                 }
             }
         }
         public static void UpdateData(ref string err)
         {
-            SqlConnection sqlConnection = DBase.DataBase.GetConnection();
-            try
+            using (SqlConnection connection = DBase.DataBase.GetConnection())
             {
-                sqlConnection.Open();
-                var req = "UPDATE aviondeaf SET immat = 'CSI-Update1', typeAvion = 'typeAvion-Update' ";
-                req += " WHERE nbHVol = 13";
-                SqlCommand sqlCommand = new SqlCommand(req, sqlConnection);
-                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+                connection.Open();
 
-                sqlDataAdapter.UpdateCommand = sqlCommand;
-                sqlDataAdapter.UpdateCommand.ExecuteNonQuery();
-                sqlCommand.Dispose();
-            }
-            catch (Exception ex)
-            {
-                err = ex.ToString();
-                throw new Exception(err);
-            }
-            finally
-            {
-                if (sqlConnection != null)
+                SqlCommand command = connection.CreateCommand();
+                SqlTransaction transaction;
+
+                // Start a local transaction.
+                transaction = connection.BeginTransaction("SampleTransaction");
+
+                // Must assign both transaction object and connection 
+                // to Command object for a pending local transaction
+                command.Connection = connection;
+                command.Transaction = transaction;
+
+                var req = "UPDATE aviondeaf SET immat = 'CSI-Update1', typeAvion = 'typeAvion-Update' ";
+                req += " WHERE nbHVol = 15";
+
+                try
                 {
-                    sqlConnection.Close();
+                    command.CommandText = req;
+                    command.ExecuteNonQuery();
+
+                    // Attempt to commit the transaction.
+                    transaction.Commit();
+                    Console.WriteLine("Both records are written to database.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Commit Exception Type: {0}", ex.GetType());
+                    Console.WriteLine("  Message: {0}", ex.Message);
+
+                    // Attempt to roll back the transaction. 
+                    try
+                    {
+                        transaction.Rollback();
+                    }
+                    catch (Exception ex2)
+                    {
+                        // This catch block will handle any errors that may have occurred 
+                        // on the server that would cause the rollback to fail, such as 
+                        // a closed connection.
+                        Console.WriteLine("Rollback Exception Type: {0}", ex2.GetType());
+                        Console.WriteLine("  Message: {0}", ex2.Message);
+                    }
                 }
             }
         }
